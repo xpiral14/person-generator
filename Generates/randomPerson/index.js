c2752b932ab732a94c4a8966fe4ae1e42d0f4db4
Samuel Reis <sa.reis1999@gmail.com>
2019-08-11 18:28:52 -0300

    Organização dos dados

--- Generates/randomPerson/index.js
+++ Generates/randomPerson/index.js
@@ -1,18 +1,39 @@
 const fullName = require('../randomFullName')
 const randomTel = require('../randomTel')
 const cityState = require('../randomCity&State')
-
-function randomPerson(){
+/**
+ * Create as many people as you need
+ * @param {number} howmany : howmany persons you want create
+ * 
+ */
+function randomPerson(howmany) {
+    if (isNaN(howmany) || howmany < 1) {
+        throw 'Insira um número válido!'
+    }
     let place = cityState()
     let name = fullName()
-    let person = {
-        name: name.nome,
-        sexo: name.sexo,
-        tel: randomTel(),
-        estado: place.estado,
-        cidade: place.cidade
+    let person;
+    if (howmany === 1) {
+        person = {
+            name: name.nome,
+            sexo: name.sexo,
+            tel: randomTel(),
+            estado: place.estado,
+            cidade: place.cidade
+        }
+    } else {
+        person = []
+        for (let i = 0; i < howmany.length; i++) {
+            person.push({
+                name: name.nome,
+                sexo: name.sexo,
+                tel: randomTel(),
+                estado: place.estado,
+                cidade: place.cidade
+            })
+        }
     }
     return person
 }
 
-console.log(randomPerson());
+module.exports = randomPerson
--- Person/Names/index.js
+++ Person/Names/index.js
@@ -1,6 +1,6 @@
-let maleNames = ['Samuel', 'Erick', 'André', 'Felipe', 'Diego', 'Marcos', 'Luciano', 'Álvaro', 'Alexandre', 'Ricardo', 'Vinicius', 'Arthur', 'Thalles', 'Iago', 'Caio', 'João', 'Vitor', 'César', 'Lucas', 'Alex', 'Mateus'].map(nome => ({nome, sexo: 'm'}))
+let maleNames = ['Samuel', 'Erick', 'André', 'Felipe', 'Diego', 'Marcos', 'Luciano', 'Álvaro', 'Alexandre', 'Ricardo', 'Vinicius', 'Arthur', 'Thalles', 'Iago', 'Caio', 'João', 'Vitor', 'César', 'Lucas', 'Alex', 'Mateus', 'Renato', 'Pedro', 'Rodrigo', 'Jonas', 'Jenrique', 'Fernando', 'Vicente', 'Rômulo', 'Felipe', 'Diego', 'Daniel', 'Michel', 'Fabricio'].map(nome => ({nome, sexo: 'm'}))
 
-let femaleNames = ['Sabrina', 'Fátima', 'Luciana', 'Jade', 'Claudinéia', 'Maria', 'Joana', 'Amanda', 'Beatriz', 'Gabriela', 'Isabela'].map(nome => ({nome, sexo: 'f' }))
+let femaleNames = ['Sabrina', 'Fátima', 'Luciana', 'Jade', 'Claudinéia', 'Maria', 'Joana', 'Amanda', 'Beatriz', 'Gabriela', 'Isabela', 'Brenda', 'Jenifer', 'Luíza', 'Patricia', 'Assunção', 'Eduarda', 'Michelle', 'Fabricia', 'Daniela', 'Isabel', 'Cláudia'].map(nome => ({nome, sexo: 'f' }))
 
 let anyName = [...maleNames, ...femaleNames]
 
